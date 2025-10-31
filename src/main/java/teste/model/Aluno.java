643c96d24e7626f9d9d1f170c28627b36621b57e
Belufi <belufi.daniel@aluno.ifsp.edu.br>
2025-10-30 22:06:35 -0300

    Muitas coisas

--- src/main/java/teste/model/Aluno.java
+++ src/main/java/teste/model/Aluno.java
@@ -1,12 +1,13 @@
 package teste.model;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+import javax.persistence.ManyToOne;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -31,10 +32,13 @@ public class Aluno implements Serializable {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;	
 	private String prontuario;
-	private String frase;
 	private String nome;
 	private String sobrenome;
 	private Boolean boo;
-	
-	
+	private Date datanascimento;
+	private String telefone;
+	private float salario;
+
+	@ManyToOne
+	private Professor professor;
 }
